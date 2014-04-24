2539c70147efb099908cef0bf4c7408b6ea220ad
He Li <jet.in.brain@gmail.com>
2014-04-24 22:23:46 +0800

    add post service

---
+++ src/main/java/com/fan1tuan/client/user/business/PostService.java
@@ -0,0 +1,13 @@
+package com.fan1tuan.client.user.business;
+
+import com.fan1tuan.business.support.Fan1TuanService;
+
+/**
+ * Created by Li He on 2014/4/21.
+ * <p></p>
+ * @author Li He
+ */
+public interface PostService extends Fan1TuanService{
+
+
+}
